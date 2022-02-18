b5b45718516d1f80fab5f0a641744e607ab14362
xuxinyuan <204663476@qq.com>
2022-02-18 17:28:03 +0800

    添加input和search图标

--- components/SearchBox.vue
+++ components/SearchBox.vue
@@ -1,34 +1,56 @@
 <template>
-  <div class="flex h-10 w-80 bg-gray-100">
-    <a class="px-4 text-sm inline-flex">
-      <span class="custom-font-14 leading-10 relative inline"
-        >工具<down/></span>
+  <div class="flex h-8 w-72 bg-gray-100 relative">
+    <a
+      class="px-4 text-sm inline-flex select-none cursor-pointer"
+    >
+      <span class="custom-font-14 leading-8 relative inline"
+        >工具<down theme="filled"
+      /></span>
     </a>
-    <span class=" m-2 custom-line-right"></span>
+    <span class="m-2 custom-line-right"></span>
+    <input
+      class="
+        w-40
+        bg-gray-100
+        border-0 border-transparent
+        focus:outline-none
+        custom-font-14
+        text-gray-600
+      "
+      v-model="searchKey"
+      type="text"
+    />
+    <search class="ml-2 cursor-pointer" @click="search()"/>
   </div>
 </template>
 
 <script>
-import { Down } from "@icon-park/vue";
+import { Down, Search } from "@icon-park/vue/lib";
 
 export default {
   components: {
     Down,
+    Search,
   },
 
   name: "SearchBox",
   data() {
-    return {};
+    return {
+      searchType: "",
+      searchKey: "",
+    };
   },
 
   mounted() {},
+  methods: {
+    search() {
+      console.log("searchKey ==> " + this.searchKey);
+    },
+  },
 };
 </script>
 
 <style scoped>
-.custom-font-14 {
-  font-size: 14px;
-}
 .custom-line-right {
   border-right: 1px solid #dee1e4;
 }
--- components/Toolbar.vue
+++ components/Toolbar.vue
@@ -1,19 +1,20 @@
 <template>
-  <header>
-    <div class="flex p-4 justify-between items-center">
+  <header class="shadow-xl">
+    <div class="flex px-6 py-3 justify-between items-center">
       <div class="flex items-center">
-        <div class="font-black inline text-3xl">ZNGG</div>
-        <ul class="text-base ml-4 inline items-center">
-          <li class="inline mx-2">首页</li>
-          <li class="inline mx-2">工具</li>
-          <li class="inline mx-2">文章</li>
-          <li class="inline mx-2">源码</li>
+        <div class="font-black inline text-xl cursor-pointer"><img src="logo-black.png" class="w-20 h-8" alt="" srcset=""></div>
+        <ul class="menu ml-4 items-center hidden md:inline custom-font-14 pl-4">
+          <li class="inline mx-2 cursor-pointer select-none active">首页</li>
+          <li class="inline mx-2 cursor-pointer select-none">工具</li>
+          <li class="inline mx-2 cursor-pointer select-none">文章</li>
+          <li class="inline mx-2 cursor-pointer select-none">源码</li>
         </ul>
       </div>
       <div class="inline">
-        <search-box />
+        <search-box class="hidden md:block" />
       </div>
     </div>
+    <login />
   </header>
 </template>
 
@@ -26,5 +27,9 @@ export default {
 <style scoped>
 header {
   background: white;
+}
+.menu>li:hover,.active {
+  color: #3955f6;
+  font-weight: 900;
 }
 </style>>
